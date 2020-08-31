bfe73060cd6e44f739f8c4eda55483687a25c87d
flaviumarasescu <50264537+flaviumarasescu@users.noreply.github.com>
2020-08-31 20:37:38 +0300

    Update script.js

--- CRUD/script.js
+++ CRUD/script.js
@@ -1,16 +1,20 @@
-var selectedRow = null
+let selectedRow = null // NOTE:
 
-function onFormSubmit(){
-  var formData = readFormData();
+document.getElementById("submit").addEventListener("click", function formSubmit() {
+  event.preventDefault();
+  let formData = readFormData();
   if (selectedRow == null)
-      insertNewRecord(formData);
-      else
-        updateRecord(formData);
+    insertNewRecord(formData);
+  else
+    updateRecord(formData);
   resetForm();
-}
+})
 
-function readFormData(){
-  var formData = {};
+
+
+
+function readFormData() {          // NOTE:citeste valorile din formular
+  let formData = {};
   formData["Nume"] = document.getElementById("Nume").value;
   formData["Cod"] = document.getElementById("Cod").value;
   formData["Salariu"] = document.getElementById("Salariu").value;
@@ -18,24 +22,24 @@ function readFormData(){
   return formData;
 }
 
-function insertNewRecord(data){             
-    var table = document.getElementById("listaAngajati").getElementsByTagName('tbody')[0];
-    var newRow = table.insertRow(table.length);
-    cell1 = newRow.insertCell(0);
-    cell1.innerHTML = data.Nume;
-    cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.Cod;
-    cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.Salariu;
-    cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.Oras;
-    cell4 = newRow.insertCell(4);
-    cell4.innerHTML = `<button type="button"  onClick="onEdit(this)"  >Update</button>
+function insertNewRecord(data) {             // NOTE: Crearea celulelor si inserarea valorilor in celelulele create
+  let table = document.getElementById("listaAngajati").getElementsByTagName('tbody')[0];          // NOTE: obiectul "table" primeste noile elemente care vor fi adaugate
+  let newRow = table.insertRow(table.length);
+  cell1 = newRow.insertCell(0);
+  cell1.innerHTML = data.Nume;
+  cell2 = newRow.insertCell(1);
+  cell2.innerHTML = data.Cod;
+  cell3 = newRow.insertCell(2);
+  cell3.innerHTML = data.Salariu;
+  cell4 = newRow.insertCell(3);
+  cell4.innerHTML = data.Oras;
+  cell4 = newRow.insertCell(4);
+  cell4.innerHTML = `<button type="button"  onClick="onEdit(this)"  >Update</button>                                                  
                       <button type="button" onClick="onDelete(this)">Delete</button>` ;
-
+  // NOTE: `` in loc de "" sau '' pentru a putea scrie pe mai multe linii asad
 }
 
-function resetForm(){
+function resetForm() {
   document.getElementById("Nume").value = "";
   document.getElementById("Cod").value = "";
   document.getElementById("Salariu").value = "";
@@ -44,14 +48,14 @@ function resetForm(){
 }
 
 function onEdit(td) {
-    selectedRow = td.parentElement.parentElement;
-    document.getElementById("Nume").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("Cod").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("Salariu").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("Oras").value = selectedRow.cells[3].innerHTML;
+  selectedRow = td.parentElement.parentElement;
+  document.getElementById("Nume").value = selectedRow.cells[0].innerHTML;
+  document.getElementById("Cod").value = selectedRow.cells[1].innerHTML;
+  document.getElementById("Salariu").value = selectedRow.cells[2].innerHTML;
+  document.getElementById("Oras").value = selectedRow.cells[3].innerHTML;
 }
 
-function updateRecord(formData){
+function updateRecord(formData) {
   selectedRow.cells[0].innerHTML = formData.Nume;
   selectedRow.cells[1].innerHTML = formData.Cod;
   selectedRow.cells[2].innerHTML = formData.Salariu;
@@ -59,9 +63,9 @@ function updateRecord(formData){
 }
 
 function onDelete(td) {
-    if (confirm('Vrei sa stergi inregistrarea ?')) {
-        var  row = td.parentElement.parentElement;
-        document.getElementById("listaAngajati").deleteRow(row.rowIndex);
-        resetForm();
-    }
+  if (confirm('Vrei sa stergi inregistrarea ?')) {
+    let row = td.parentElement.parentElement;
+    document.getElementById("listaAngajati").deleteRow(row.rowIndex);
+    resetForm();
+  }
 }
